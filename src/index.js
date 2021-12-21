03ce8ce80102805818cfce3eb376dcc48b501293
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-12-21 12:11:15 -0600

    Add like button and style it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+// import './style.css';
 import displayShows from './displayShows.js';
 
 displayShows();
--- src/makeCard.js
+++ src/makeCard.js
@@ -2,8 +2,13 @@ const makeCard = (show) => {
   const showCard = document.createElement('li');
   showCard.innerHTML = `
     <img src="${show.image.medium}">
-    ${show.name}
-    <input type="button" value="Comment">
+    <div class="title">
+      <h3>${show.name}</h3>
+      <input class="likes check" type="checkbox" name="likes" id="likes">
+      <label class="likes label" for="likes">&#9825</label>
+      <input class="likes number" type="text" value="78" disabled>
+    </div>
+    <input class="comment button" type="button" value="Comment">
   `;
   return showCard;
 };
