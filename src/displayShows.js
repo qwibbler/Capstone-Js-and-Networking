42d9c1e2eefa021fb1ab905904fd5f0137558d81
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-12-22 11:36:16 -0600

    Show shows count and pg number

--- src/displayShows.js
+++ src/displayShows.js
@@ -2,14 +2,19 @@ import makeCard from './makeCard.js';
 import mapLikes from './mapLikes.js';
 
 const displayShows = async () => {
+  const page = document.querySelector('#pg-no');
+  const count = document.querySelector('#count');
   const showsLikes = await mapLikes();
   const showsWrapper = document.querySelector('.shows-wrapper');
   const frag = document.createDocumentFragment();
+
   showsLikes.forEach((show) => {
     const card = makeCard(show);
     frag.appendChild(card);
   });
   showsWrapper.appendChild(frag);
+
+  count.textContent = `(${showsLikes.length})`;
   return showsLikes;
 };
 export default displayShows;
