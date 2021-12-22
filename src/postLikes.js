95870042dd690d8a264ebe297c99f003ad66c533
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-12-21 18:25:14 -0600

    Add postLikes func

---
+++ src/postLikes.js
@@ -0,0 +1,12 @@
+const appId = 'exGrmnNXlgE2dfxvkk5S';
+const postLikes = async (show) => {
+  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: show.id
+  	}),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  return response.text();
+};
+export default postLikes;
